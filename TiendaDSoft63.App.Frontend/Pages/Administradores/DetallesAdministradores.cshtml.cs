9fdc6dddc6d6a2b68a23d27cf8ddc1f480a8ae95
NataliaYacumal <natali3582@gmail.com>
2022-09-30 17:51:11 -0500

    versón 8.3 Front + Back

--- TiendaDSoft63.App.Frontend/Pages/Administradores/DetallesAdministradores.cshtml.cs
+++ TiendaDSoft63.App.Frontend/Pages/Administradores/DetallesAdministradores.cshtml.cs
@@ -1,12 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using TiendaDSoft63.App.Dominio;
+using TiendaDSoft63.App.Persistencia;
 namespace TiendaDSoft63.App.Frontend.Pages
 {
     public class DetallesAdministradoresModel : PageModel
     {
+        private static IRepositorioAdministrador _repoAdministrador = new RepositorioAdministrador(new Persistencia.AppContext());
+        public IEnumerable<Administrador> listaAdministradores {get; set;}
+
+        public DetallesAdministradoresModel(IEnumerable<Administrador> listaAdministradores)
+        {
+            this.listaAdministradores = listaAdministradores;
+        }
+
         public void OnGet()
         {
+            listaAdministradores = _repoAdministrador.GetAllAdministradores();
         }
+        
     }
 }
